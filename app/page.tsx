954a46fe36f009e42d3e75dd5170e82a2f71cfe5
darcivaliente <darcivaliente@gmail.com>
2025-03-05 14:00:26 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import Textarea from "react-textarea-autosize";
 import Image from "next/image";
 
 const examples = [
-  "Yeehaw, tell our Wild West Product team what to build",
-  "Saddle up, I need a summary of the latest customer feedback from the last week",
-  "Lasso me up a cowboy pun I can use down at the O.K. Corral",
+  "🌵 Yeehaw, tell our Wild West Product team what to build",
+  "🏜️ Saddle up, I need a summary of the latest customer feedback from the last week",
+  "🐴 Lasso me up a cowboy pun I can use down at the O.K. Corral",
 ];
 
 export default function Chat() {
